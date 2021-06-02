576b89206697feb7c1d8e0c081cb5ad98d854c46
guilhembn <buisanguilhem@gmail.com>
2021-06-02 23:55:44 +0200

    Starts styling and layout

--- web/index.php
+++ web/index.php
@@ -1,4 +1,14 @@
 <?php
 include "private/addresses.php";
+include "private/header.php";
+?>
+<body>
+<div id="particles-js">
+    <div id="overlay">This is a testttttttttttttttttttttttttttttttttttttttttttttttttttt<br />
+    plop plip plop<br />
+    nanani nanana</div>
+</div>
 
-echo "plop" ?>
+<script src="lib/particles.min.js"></script>
+<script src="static/particleapp.js"></script>
+</body>
---
+++ web/private/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Omega</title>
+  <meta name="description" content="">
+  <meta name="author" content="Guilhem Buisan" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <link rel="stylesheet" media="screen" href="static/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
