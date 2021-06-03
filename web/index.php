5b094cb1853df3ea0447f451a8241498b168b9c5
guilhembn <gbuisan@laas.fr>
2021-06-03 18:31:52 +0200

    [Web] Updates style

--- web/index.php
+++ web/index.php
@@ -4,9 +4,11 @@ include "private/header.php";
 ?>
 <body>
 <div id="particles-js">
-    <div id="overlay">This is a testttttttttttttttttttttttttttttttttttttttttttttttttttt<br />
-    plop plip plop<br />
-    nanani nanana</div>
+    <div id="overlay">
+        This is a testttttttttttttttttttttttttttttttttttttttttttttttttttt<br />
+        plop plip plop<br />
+        nanani nanana
+    </div>
 </div>
 
 <script src="lib/particles.min.js"></script>
