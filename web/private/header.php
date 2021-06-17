050e445cf7ee9a1ad6d9d59c86830c083c357d1c
guilhembn <buisanguilhem@gmail.com>
2021-06-17 23:18:04 +0200

    Adds favicon

--- web/private/header.php
+++ web/private/header.php
@@ -7,5 +7,6 @@
   <meta name="author" content="Guilhem Buisan" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <link rel="stylesheet" media="screen" href="static/style.css">
+    <link rel="shortcut icon" type="image/png" href="static/favicon.png"/>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
