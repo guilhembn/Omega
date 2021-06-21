6a9796763c1b29a597115c81cb7f58f620262191
guilhembn <buisanguilhem@gmail.com>
2021-06-22 00:05:53 +0200

    [Web] Fixes resources links

--- web/private/header.php
+++ web/private/header.php
@@ -6,7 +6,7 @@
   <meta name="description" content="">
   <meta name="author" content="Guilhem Buisan" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  <link rel="stylesheet" media="screen" href="static/style.css">
-    <link rel="shortcut icon" type="image/png" href="static/favicon.png"/>
+  <link rel="stylesheet" media="screen" href="/gbuisan/static/style.css">
+    <link rel="shortcut icon" type="image/png" href="/gbuisan/static/favicon.png"/>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
