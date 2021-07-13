a0db9ec21182b384dbdc9126544af79bb9e8dff5
guilhembn <gbuisan@laas.fr>
2021-07-13 19:24:30 +0200

    [Enigma] Adds run/stop check

--- omega/include/omega/enigma.hpp
+++ omega/include/omega/enigma.hpp
@@ -1,16 +1,39 @@
 #ifndef ENIGMA_HPP
 #define ENIGMA_HPP
 #include <ros/ros.h>
+#include <std_msgs/Bool.h>
 
 class Enigma{
     public:
-    Enigma(ros::NodeHandlePtr nh): nh_(nh){
+    Enigma(ros::NodeHandlePtr nh): nh_(nh), isRunStopEnabled_(false){
+        runStopPub_ = nh_->subscribe("/pr2_ethercat/motors_halted", 1, &Enigma::onRunStop, this);
+    };
+
+    void onRunStop(const std_msgs::BoolConstPtr& msg){
+        if (isRunStopEnabled_ && !(msg->data)){
+            std::cout << "Désactivation du run/stop détectée." << std::endl;
+            throw std::runtime_error("Arrêt du système Omega");
+        }
+        isRunStopEnabled_ = msg->data;
     }
 
     virtual void run() = 0;
     virtual std::string name() = 0;
 
     protected:
+    bool waitForRunStop(double timeout){
+        for (unsigned int i=0; i<timeout*100; i++){
+            if (isRunStopEnabled_){
+                break;
+            }
+            ros::Duration(0.01).sleep();
+            ros::spinOnce();
+        }
+        return isRunStopEnabled_;
+    }
+
     ros::NodeHandlePtr nh_;
+    ros::Subscriber runStopPub_;
+    bool isRunStopEnabled_;
 };
 #endif /* ENIGMA_HPP */
