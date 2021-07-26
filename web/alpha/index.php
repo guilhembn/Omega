a0ba04320c3d0ab229a7ddd3b576145cf81a0970
guilhembn <gbuisan@laas.fr>
2021-07-26 18:38:12 +0200

    [Alpha] Move to alpha folder

---
+++ web/alpha/index.php
@@ -0,0 +1,20 @@
+<?php
+include "../private/addresses.php";
+include "../private/header.php";
+?>
+<body>
+<div id="particles-js">
+    <div class="overlay incremental">
+        <p>Félicitations !</p>
+        <p>Vous avez résolu l'énigme !</p>
+        <p>La récompense se trouve...</p>
+        <p>Oh, mais la partie ne fait que commencer</p>
+        <p>Bon courage.</p>
+        <p><a target="_blank" rel="noopener noreferrer" href="tresor.png">Projet</a> <a target="_blank" rel="noopener noreferrer" href="OMEGA2021.pdf">Omega</a> <a target="_blank" rel="noopener noreferrer" href="rosbag.bag">lancé.</a></p>
+    </div>
+</div>
+
+<script src="../lib/particles.min.js"></script>
+<script src="../static/particleapp.js"></script>
+<script src="../static/incremental.js"></script>
+</body>
