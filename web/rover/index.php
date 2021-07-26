a7e18ad6754da8b285f9a746eb84b7d1373dee76
guilhembn <gbuisan@laas.fr>
2021-07-26 18:39:21 +0200

    [Web] Adds rover

---
+++ web/rover/index.php
@@ -0,0 +1,20 @@
+<?php
+include "../private/header.php";
+?>
+<body>
+<div id="particles-js">
+    <div class="overlay">
+        Instructions: <br /><br />
+        <div style="text-align: left;">
+            1. Démarrer PR2<br />
+            2. Depuis la session de <i>gsarthou</i>, initialiser le robot <i>(robot.launch)</i> <br />
+            3. Depuis un autre terminal, toujours sur la session de <i>gsarthou</i>, workspace <i>sherlock</i> : <a href="omega.launch"><i>roslaunch omega.launch</i></a><br />
+            4. Entrer le mot de passe. (Note : le premier est indiqué sous le clavier de targaryen)<br />
+        </div>
+    </div>
+</div>
+
+<script src="../lib/particles.min.js"></script>
+<script src="../static/particleapp.js"></script>
+<script src="../static/incremental.js"></script>
+</body>
